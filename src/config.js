f6a0d392402e028605567a34eac4fd1ccc59b15f
yauheni-korbut <yauhenikorbut@gmail.com>
2024-01-22 17:56:23 +0100

    update

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const protocol = 'https://';
 
-const host = 'philosophical-ethelind-yauheni.koyeb.app';
+const host = 'philosophical-ethelind-yauheni.koyeb.app/api';
 
 export const apiUrl = `${protocol}${host}`;
